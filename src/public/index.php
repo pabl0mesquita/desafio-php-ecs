738e08ea1286b65f5c07a0f265d06a7599924cbe
desenvolvedor33 <desenvolvedor33@webcartorios.com.br>
2025-02-02 19:15:18 -0300

    rodando a aplicação teste

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,14 @@
 <?php 
 
 require_once __DIR__.'/../vendor/autoload.php';
-
-use Core\Database\Connection;
+// use Core\Database\Connection;
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
 
 $dotenv->load();
 
 try {
-    $con = Connection::getInstance();
-    var_dump($con);
+    // $con = Connection::getInstance();
+    // var_dump($con);
     echo "Rodando...";
 
 }catch(Exception $e) {
